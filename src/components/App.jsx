dad2b3f5fa73611cf0a435ef5c5fb49b41fa0b63
ArturDatsko <133085603+ArturDatsko@users.noreply.github.com>
2023-12-18 22:07:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,28 @@
-import { StyledTitle } from './Filter/Filter.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchContacts } from 'api/contacts-api';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-
 import {
   selectFilters,
   selectContacts,
   selectIsLoading,
   selectError,
 } from '../redux/selectors';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { StyledTitle } from './Filter/Filter.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'api/contacts-api';
 
 export const App = () => {
   const filter = useSelector(selectFilters);
   const contacts = useSelector(selectContacts);
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -29,6 +33,7 @@ export const App = () => {
       <h2>Phonebook</h2>
       <ContactForm />
       <h2>Contacts</h2>
+      <Filter />
       {error && (
         <div>
           <b>Error! Please reload the page or try later!</b>
@@ -39,10 +44,10 @@ export const App = () => {
           <b>Loading...</b>
         </div>
       )}
-      {contacts.length ? (
+      {filteredContacts.length ? (
         <>
           <StyledTitle>Find contacts by name</StyledTitle>
-          <ContactList />
+          <ContactList items={filteredContacts} />
         </>
       ) : (
         <StyledTitle>There are no contacts yet!</StyledTitle>
